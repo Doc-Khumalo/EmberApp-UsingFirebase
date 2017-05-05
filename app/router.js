408a1216db2e14aac8fc76dbb7f914ea893529b4
Leslie <leslie.khumalo@icloud.com>
2017-05-05 08:52:03 +0100

    Ember init commit
    added initial files

---
+++ app/router.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('tasks');
+
+  this.resource('tasks', function() {
+    this.route('new');
+    this.route('edit', {path: '/edit/:task_id'});
+  });
+});
+
+export default Router;
